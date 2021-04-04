55cfe81c70d6c7cb050c5e11f8cdad8d0724685e
zhpeng811 <zhpeng@ualberta.ca>
2021-04-04 11:59:57 -0600

    remove target directory

--- model/target/debug/deps/model-871075d78131cabe.d
+++
@@ -1,9 +0,0 @@
-/mnt/c/Users/pengz/Documents/UofA Winter 2021/ECE 421/Project/connect4-with-toot-and-otto/model/target/debug/deps/model-871075d78131cabe: src/main.rs src/disc.rs src/board.rs src/player.rs src/game.rs
-
-/mnt/c/Users/pengz/Documents/UofA Winter 2021/ECE 421/Project/connect4-with-toot-and-otto/model/target/debug/deps/model-871075d78131cabe.d: src/main.rs src/disc.rs src/board.rs src/player.rs src/game.rs
-
-src/main.rs:
-src/disc.rs:
-src/board.rs:
-src/player.rs:
-src/game.rs:
--- model/target/debug/deps/model-9966141e9ba30096.d
+++
@@ -1,6 +0,0 @@
-/mnt/c/Users/pengz/Documents/UofA Winter 2021/ECE 421/Project/connect4-with-toot-and-otto/model/target/debug/deps/model-9966141e9ba30096: src/main.rs src/connect4.rs
-
-/mnt/c/Users/pengz/Documents/UofA Winter 2021/ECE 421/Project/connect4-with-toot-and-otto/model/target/debug/deps/model-9966141e9ba30096.d: src/main.rs src/connect4.rs
-
-src/main.rs:
-src/connect4.rs:
