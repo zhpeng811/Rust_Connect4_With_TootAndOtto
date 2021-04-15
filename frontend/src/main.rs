0beb760bef10bc1ede1f0f3b3d89cf50d3426d90
Ze Hui Peng <zhpeng@ualberta.ca>
2021-04-15 12:22:13 -0600

    make text more general

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -123,7 +123,7 @@ impl Model {
 
                 // <!-- Top menu on small screens -->
                 <header class="w3-container w3-top w3-hide-large w3-red w3-xlarge w3-padding">
-                    <div align="center"> {"Connect 4 with Rust"} </div>
+                    <div align="center"> {"Board Games with Rust"} </div>
                 </header>
 
                 // <!-- Overlay effect when opening sidenav on small screens -->
